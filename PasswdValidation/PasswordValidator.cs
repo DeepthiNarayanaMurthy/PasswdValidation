a2e9d6d2eca4c27b34e103e155ad4e3ed416ab28
DeepthiNarayanaMurthy <deepthi.narayanamurthy@tieto.com>
2017-01-16 17:53:59 +0530

    Password Validation Application

---
+++ PasswdValidation/PasswordValidator.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+
+namespace PasswordValidation
+{
+    public class PasswordValidator
+    {
+        public bool Verify(string password, string UserType)
+        {
+            if (UserType == "Internal")
+            {
+                InternalPasswordValidator obj = new InternalPasswordValidator();
+                bool result = obj.Verify(password);
+                return result;
+            }
+            else if (UserType == "External")
+            {
+                ExternalPasswordValidator obj = new ExternalPasswordValidator();
+                bool result = obj.Verify(password);
+                return result;
+            }
+            else
+                throw new InvalidPasswdException("Not a valid user type");
+
+        }
+    }
+    public class InternalPasswordValidator:PasswordValidator
+    {
+        public bool Verify(string password)
+        {
+            if (password.Length <= 8)
+            {
+                    throw new InvalidPasswdException("Password should be atleast 8 in length ");
+            }
+            else
+            {
+                    return true;
+            }
+        }
+
+    }
+    public class ExternalPasswordValidator:PasswordValidator
+    {
+        int count=0;
+        public bool Verify(string password)
+        {
+            if (!CheckNull(password) || !CheckUppercase(password))
+            {
+                return false;
+            }
+            else if (!CheckLowercase(password)&&!CheckLength(password) && !CheckDigit(password))
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+        public bool CheckNull(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                throw new InvalidPasswdException("Password cannot be empty");
+            else
+            {
+                count++;
+                return true;
+            }
+
+        }
+        public bool CheckUppercase(string password)
+        {
+            if (password.Where(char.IsUpper).Count() < 1)
+            {
+                throw new InvalidPasswdException("Password should have atleast one Uppercase");
+
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public bool CheckLength(string password)
+        {
+            if (password.Length <= 8)
+            {
+                throw new InvalidPasswdException("Password should be atleast 8 in length ");
+            }
+            else
+            {
+                count++;
+                return true;
+            }
+        }
+        public bool CheckLowercase(string password)
+        {
+            if (password.Where(char.IsLower).Count() < 1)
+            {
+                throw new InvalidPasswdException("Password should be atleast 1 lowercase ");
+            }
+            else
+            {
+                count++;
+                return true;
+            }
+        }
+        public bool CheckDigit(string password)
+        {
+            if (password.Where(char.IsDigit).Count() < 1)
+            {
+                throw new InvalidPasswdException("Password should be atleast 1 digit ");
+            }
+            else
+            {
+                count++;
+                return true;
+            }
+        }
+    }
+
+    public class InvalidPasswdException : Exception
+    {
+        public InvalidPasswdException(string message):base(message)
+        {
+
+        }
+    }
+}
---
+++ PasswordValidatorTest/UnitTest1.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordValidation;
+
+namespace PasswordValidatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private bool ConstructAndCall(string password,string UserType)
+        {
+            PasswordValidator Password = new PasswordValidator();
+            bool result = Password.Verify(password,UserType);
+            return result;
+        }
+        [TestMethod]
+        public void GivingNullStringAsPassword_VerifyingIt_ShouldThrowException()
+        {
+            ExternalPasswordValidator Password = new ExternalPasswordValidator();
+            try
+            {
+                bool expected = Password.CheckNull("");
+            }
+            catch(InvalidPasswdException ex)
+            {
+                Assert.AreEqual("Password cannot be empty", ex.Message, "Null password exception raised");
+            }
+        }
+
+        [TestMethod]
+        public void GivingNoUppercaseCharacter_VerifyingIt_ThrowsException()
+        {
+            ExternalPasswordValidator Password = new ExternalPasswordValidator();
+            try
+            {
+                bool expected = Password.CheckUppercase("sadadecd1");
+            }
+            catch (InvalidPasswdException ex)
+            {
+                Assert.AreEqual("Password should have atleast one Uppercase", ex.Message, "No Uppercase exception raised");
+            }
+        }
+        [TestMethod, ExpectedException(typeof(InvalidPasswdException))]
+        public void GivingPasswordOfLengthLessThanEight_VerifyIt_ThrowsException()
+        {
+            ExternalPasswordValidator Password = new ExternalPasswordValidator();
+            bool expected = Password.CheckLength("KIJU");           
+        }
+
+        [TestMethod,ExpectedException(typeof(InvalidPasswdException))]
+        public void GivingNoLowercaseCharacter_VerifyingIt_ThrowsException()
+        {
+            ExternalPasswordValidator Password = new ExternalPasswordValidator();
+            bool expected = Password.CheckLowercase("ASD");          
+        }
+       
+
+
+        [TestMethod, ExpectedException(typeof(InvalidPasswdException))]
+        public void GivingNoNumberInPassword_VerifyingIt_ThrowsException()
+        {
+            ExternalPasswordValidator Password = new ExternalPasswordValidator();
+            bool expected = Password.CheckDigit("ASD");       
+        }
+        
+        [TestMethod, ExpectedException(typeof(InvalidPasswdException))]
+        public void GivingPasswordWithLessThanThreeConditions_ForExternal_VerifyIt_ThrowsException()
+        {
+            bool expected = ConstructAndCall("WESDASXD", "External");
+        }
+
+        [TestMethod]
+        public void GivingPasswordWithGreaterThanThreeCondition_ForExternal_VerifyIt_ThrowsException()
+        {
+            bool expected = ConstructAndCall("aaAdadaas", "External");
+            Assert.AreEqual(true, expected, "Correct password");
+
+        }
+        [TestMethod]
+        public void GivingPasswordWithAllCondition_ForExternal_VerifyIt_ReturnsTrue()
+        { 
+            bool expected = ConstructAndCall("aWadaascd12","External");
+            Assert.AreEqual(true, expected, "Correct password");
+        }
+
+        [TestMethod]
+        public void GivingPasswordWithMoreThanEightCharacter_ForInternal__VerifyIt_ReturnsTrue()
+        {
+            bool expected = ConstructAndCall("aWadaascd", "Internal");
+            Assert.AreEqual(true, expected, "Correct password");
+        }
+
+        [TestMethod]
+        public void GivingPasswordWithLessThanEightCharacter_ForInternal__VerifyIt_ThrowsException()
+        {
+            try
+            {
+                bool expected = ConstructAndCall("aWadaascd", "Internal");
+            }
+            catch (InvalidPasswdException ex)
+            {
+                Assert.AreEqual("Password should be atleast 8 in length", ex.Message, "Invalid exception raised");
+            }
+            
+        }
+
+
+    }
+}
